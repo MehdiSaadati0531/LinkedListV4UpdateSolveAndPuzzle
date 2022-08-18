6419196002972537a4f5c5f2d4ac0221101e88d0
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-18 13:39:20 +0430

    Add files via upload

---
+++ LinkedListV4UpdateSolve/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SortLinkedList;
+
+Console.WriteLine("Hello, World!");
+
+
+Operation a = new Operation();
+
+a.Enqueue(1);
+a.Enqueue(2);
+a.Enqueue(3);
+a.Enqueue(4);
+
+a.printpuzzle();
